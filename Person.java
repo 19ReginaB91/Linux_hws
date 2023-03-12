c404a609445e7d360502527f437ee0b494d20926
19ReginaB91 <126571780+19ReginaB91@users.noreply.github.com>
2023-03-12 21:06:57 +0100

    Person Java
    Java Home Work.0

---
+++ Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    private static int Regina;
+
+    public static void main(String[] args) {
+        String name = "Regina";
+        String lastName = "Batiridou";
+        int yearOfBirth = 1991;
+        int age = 31;
+        float height = 172F;
+        String sex = "Female";
+        boolean wifeAndMom = true;
+        String hobbies = "sport, art, study";
+
+        System.out.println(lastName);
+        System.out.println(yearOfBirth);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(sex);
+        System.out.println(wifeAndMom);
+        System.out.println(hobbies);
+
+    }
+}
